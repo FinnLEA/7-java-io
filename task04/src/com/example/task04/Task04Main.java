2d726f7ee7de86a9760e5daa22c9a80f89ce6d43
FinnLEA <56691691+FinnLEA@users.noreply.github.com>
2020-11-25 16:59:24 +0500

    Веденькин task04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,6 +1,9 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Formatter;
+import java.util.Locale;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -8,7 +11,23 @@ public class Task04Main {
         // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
         // - запустить программу
         // - проверить, что получилось 351.731900
+        double result = 0.0;
+        StringBuilder stringBuilder = new StringBuilder();
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
 
-        System.out.println("0.0");
+        while (inputStreamReader.ready()){
+            stringBuilder.appendCodePoint(inputStreamReader.read());
+        }
+
+        String[] parseStrs = stringBuilder.toString().split("\\s");
+
+        for (String number : parseStrs){
+            try {
+                result += Double.parseDouble(number);
+            }
+            catch (NumberFormatException exception){}
+        }
+
+        System.out.println(new Formatter(Locale.US).format("%.6f", result));
     }
 }
